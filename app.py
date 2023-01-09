012200b6d48be44c9e504c482feed4c412ce96a0
CanonWiseman <wisemancanon@gmail.com>
2023-01-09 01:28:43 -0600

    added logout functionality

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 toolbar = DebugToolbarExtension(app)
 
@@ -50,8 +50,9 @@ def do_login(user):
 def do_logout():
     """Logout user."""
 
-    db.session.clear()
-    return redirect(url_for('homepage'))
+    session.clear()
+
+    return redirect('/')
 
 
 @app.route('/signup', methods=["GET", "POST"])
@@ -114,8 +115,8 @@ def login():
 def logout():
     """Handle logout of user."""
 
-    # IMPLEMENT THIS
-
+    do_logout()
+    return redirect('/')
 
 ##############################################################################
 # General user routes:
